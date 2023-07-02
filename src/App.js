19cf1be56984a93ec38260feab89d550959d35b9
lamees <lamesshalaby671@gmail.cm>
2023-07-02 17:51:54 +0300

    hellllo

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import logo from "./logo.svg";
 import "./App.css";
 // Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import Landing from "./components/Landing/Landing";
 function App() {
   return (
-    <div className="">
-      <button className=""></button>
-    </div>
+    < >
+
+    <Landing></Landing>
+
+    </>
   );
 }
 
